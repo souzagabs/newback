c60e59a951b867b53869af8fc6925ad65585d578
Gabriel Brasil <gabrielbrasil87@outlook.com>
2025-12-01 23:51:55 -0400

    update

--- src/routes/moduloRoutes.js
+++ src/routes/moduloRoutes.js
@@ -3,7 +3,12 @@ import moduloController from '../controllers/moduloController.js';
 
 const router = Router();
 
-router.get('/:cursoId/:moduloId', moduloController.listarModulos);
+router.get('/curso/:cursoId', moduloController.listarModulos);
+router.get('/curso/:cursoId/modulo/:moduloId', moduloController.listarModuloEspecifico);
+
 router.post('/:cursoId', moduloController.criarModulo);
+router.put('/:moduloId', moduloController.atualizarModulo);
+router.delete('/:moduloId', moduloController.deletarModulo);
+router.put('/lote/:cursoId', moduloController.atualizarModulosEmLote);
 
 export default router;
